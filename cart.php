59ffb23f16f8e608cca7239355b9f2c4cd344acb
Erick Nava <erick.nava@fastcode.today>
2020-07-30 08:07:10 -0500

    updates

--- cart.php
+++ cart.php
@@ -8,11 +8,42 @@ MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
+$payer = new MercadoPago\Payer();
+$payer->name = "Charles";
+$payer->surname = "Luevano";
+$payer->email = "charles@hotmail.com";
+$payer->date_created = "2018-06-02T12:58:41.425-04:00";
+$payer->phone = array(
+    "area_code" => "",
+    "number" => "949 128 866"
+);
+
+$payer->address = array(
+    "street_name" => "Cuesta Miguel Armendáriz",
+    "street_number" => 1004,
+    "zip_code" => "11020"
+);
+
+
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
-$item->title = 'Mi producto';
-$item->quantity = 1;
+$item->id = "1234";
+$item->title = "Heavy Duty Plastic Table";
+$item->description = "Table is made of heavy duty white plastic and is 96 inches wide and 29 inches tall";
+$item->category_id = "home";
+$item->quantity = 7;
+$item->currency_id = "MXN";
 $item->unit_price = 75.56;
+
+
+
+$preference->back_urls = array(
+    "success" => "https://www.tu-sitio/success",
+    "failure" => "http://www.tu-sitio/failure",
+    "pending" => "http://www.tu-sitio/pending"
+);
+$preference->auto_return = "approved";
+
 $preference->items = array($item);
 $preference->save();
 
