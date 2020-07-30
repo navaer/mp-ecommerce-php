ebe7e36dd52fee3cfc34fe391370934e7b930046
Erick Nava <erick.nava@fastcode.today>
2020-07-30 07:51:36 -0500

    updates

--- cart.php
+++ cart.php
@@ -13,35 +13,8 @@ $item = new MercadoPago\Item();
 $item->title = 'Mi producto';
 $item->quantity = 1;
 $item->unit_price = 75.56;
-
-$payer = new MercadoPago\Payer();
-$payer->name = "Charles";
-$payer->surname = "Luevano";
-$payer->email = "charles@hotmail.com";
-$payer->date_created = "2018-06-02T12:58:41.425-04:00";
-$payer->phone = array(
-    "area_code" => "",
-    "number" => "949 128 866"
-);
-
-$payer->address = array(
-    "street_name" => "Cuesta Miguel Armendáriz",
-    "street_number" => 1004,
-    "zip_code" => "11020"
-);
-// ...
-
-
 $preference->items = array($item);
-$preference->payer = $payer;
-$preference->back_urls = array(
-    "success" => "https://www.tu-sitio/success",
-    "failure" => "http://www.tu-sitio/failure",
-    "pending" => "http://www.tu-sitio/pending"
-);
-$preference->auto_return = "approved";
 $preference->save();
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -57,7 +30,5 @@ $preference->save();
     </script>
 </form>
 
-
-<a href="<?php echo $preference->init_point; ?>">Pagar con Mercado Pago</a>
 </body>
 </html>
