71b0cc740e03c82026859bdbd5dac66ad131b1aa
Erick Nava <erick.nava@fastcode.today>
2020-07-31 19:51:01 -0500

    updates

--- webhook.php
+++ webhook.php
@@ -4,9 +4,8 @@ require_once 'vendor/autoload.php';
 $filename = $_GET['id'] . '.json';
 
 
-$data = file_get_contents('php://input');
-file_put_contents($filename, print_r($data, true));
-
+$data = file_get_contents('php://input') . PHP_EOL;
+file_put_contents($filename, print_r($data, true), FILE_APPEND);
 
 ?>
 
